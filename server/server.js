69ec86f2f48e62c8990e550f5e5dc83e38eab933
Jason R <marathon758@gmail.com>
2019-07-22 05:36:44 -0500

    connection with mysql database started

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ let app = express()
 
 console.log("Server Start");
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.PASSWORD,
+    database: 'lpt'
+});
+
 // Catch Errors
 app.use(function(err, req, res, next){
     console.log('Error at Top:');
