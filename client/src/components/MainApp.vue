b541cd21bd84a878306547ab091088c22d42839c
Jason R <marathon758@gmail.com>
2019-07-22 07:27:59 -0500

    D3.js library added to show trace data visually. Plot showing in chrome browser

--- client/src/components/MainApp.vue
+++ client/src/components/MainApp.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    Test {{ this.time_trace }}
+    <div class='plot'></div>
   </div>
 </template>
 
 <script>
 const axios = require('axios')
+import * as d3 from 'd3';
 export default {
   name: 'MainApp',
   data() {
     return {
       time_trace: null,
-      traceData: null
+      traceData: null,
+      margin: 100,
+      width: 1200,
+      height: 600,
+      line: null,
     }
   },
   methods: {
@@ -27,7 +32,33 @@ export default {
      
     },
     init: function() {
-      this.test = 'working'
+      this.dataGroup = d3.select('.plot')
+        .append('svg')
+        .attr('width', this.width + this.margin * 2)
+        .attr('height', this.height + 2 * this.margin)
+        .append('g')
+        .attr('transform', `translate(${this.margin},${this.margin})`)
+
+      this.line = d3.line()
+        .x(d => x(d.freq))
+        .y(d => y(d.amplitude))
+
+      let x = d3.scaleLinear()
+        // .domain(d3.extent(this.traceData, d => d.freq))
+        .domain([850, 1150])
+        .range([0,this.width])
+
+      let y = d3.scaleLinear()
+        // .domain(d3.extent(this.traceData, d => d.amplitude))
+        .domain([-130, -30])
+        .range([this.height, 0])
+      
+      this.dataGroup.append('path')
+        .data([this.traceData])
+        .attr('fill','none')
+        .attr('class','pathLines')
+        .attr('stroke', 'black')
+        .attr('d', this.line)
     }
   },
   mounted() {
